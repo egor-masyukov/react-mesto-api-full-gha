823a1b9c70aeed428ecbdd307d201cd40de9b332
Egor Masyukov <egor-masyukov@yandex.ru>
2023-07-14 21:42:40 +0300

    перенаправление запросов api

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
   },
   about: {
     type: String,
-    default: 'Исследователь океана',
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
